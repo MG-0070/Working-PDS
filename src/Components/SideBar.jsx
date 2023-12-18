7fae472fd6a08f1d6795ef9baebd14f46889d983
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2023-12-18 17:01:09 +0530

    SideBar.jsx

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import {
   ListItemIcon,
@@ -8,7 +8,9 @@ import {
   Typography,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import { Keyboard, BatchPrediction } from "@mui/icons-material";
+import { BatchPrediction, AddHome } from "@mui/icons-material";
+import UserContext from "../Context/userContext";
+import { useContext } from "react";
 
 const useStyles = makeStyles({
   menuItem: {
@@ -45,61 +47,216 @@ const useStyles = makeStyles({
 function SideBar() {
   const classes = useStyles();
   const navigate = useNavigate();
+  const { selectedOption, setSelectedOption } = useContext(UserContext);
+  // console.log(selectedOption)
 
-  const [selectedOption, setSelectedOption] = useState("Dashboard");
-
-  const handleOptionClick = (option) => {
+  const handleClick = (option) => {
     setSelectedOption(option);
-    navigate("/home/main1");
+
+    if (option === "Home") {
+      navigate("/dashboard/home");
+    } else if (option === "Dashboard") {
+      navigate("/dashboard");
+    }
   };
 
+  // useEffect(() => {
+  //   console.log("Selected Option:", selectedOption);
+  // }, [selectedOption]);
+
   return (
-    <div style={{ position: "fixed", height: "100vh", padding: 20 }}>
-      <div style={{ fontWeight: "bold" ,color : "#333333" }}> PDS-DASHBOARD</div>
+    <div style={{ position: "relative", height: "180vh", padding: "15px" }}>
+      <Typography
+        style={{
+          fontSize: 17,
+          marginLeft: -80,
+          fontWeight: "bold",
+          color: "#333333",
+        }}
+        variant="inherit"
+      >
+        Dashboard PDS
+      </Typography>
       <MenuList>
         <MenuItem
+          style={{ marginLeft: "2px" }}
           onClick={() => {
-            navigate("/home");
-            setSelectedOption("Dashboard");
+            navigate("/dashboard/home");
+            setSelectedOption("HomePage");
           }}
-          selected={selectedOption === "Dashboard"}
+          selected={selectedOption === "HomePage"}
           classes={{ root: classes.menuItem }}
         >
           <ListItemIcon>
-            <Keyboard fontSize="small" className={classes.icon} />
+          <AddHome fontSize="small" className={classes.icon} />
           </ListItemIcon>
           <ListItemText>
             <Typography
-              style={{ fontSize: 14, marginLeft: 20 }}
-              variant="inherit"
-            >
-              Dashboard
-            </Typography>
-          </ListItemText>
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            navigate("/home/main1");
-            setSelectedOption("Home");
-          }}
-          selected={selectedOption === "Home"}
-          classes={{ root: classes.menuItem }}
-        >
-          <ListItemIcon>
-            <BatchPrediction fontSize="small" className={classes.icon} />
-          </ListItemIcon>
-          <ListItemText>
-            <Typography
-              style={{ fontSize: 14, marginLeft: 20 }}
+              style={{
+                fontSize: 17,
+                marginLeft: -100,
+                fontWeight: "bold",
+                color: "#333333",
+              }}
               variant="inherit"
             >
               Home
             </Typography>
           </ListItemText>
         </MenuItem>
+        <MenuItem
+          onClick={() => {
+            navigate("/dashboard");
+            setSelectedOption("Punjab Intra Route Optimization For PDS");
+          }}
+          selected={selectedOption === "Punjab Intra Route Optimization For PDS"}
+          classes={{ root: classes.menuItem }}
+        >
+          <ListItemIcon>
+          <BatchPrediction fontSize="small" className={classes.icon} />
+          </ListItemIcon>
+          <ListItemText>
+            <Typography
+              style={{
+                fontSize: 17,
+                marginLeft: -75,
+                fontWeight: "bold",
+                color: "#333333",
+              }}
+              variant="inherit"
+            >
+              Dashboard
+            </Typography>
+          </ListItemText>
+        </MenuItem>
       </MenuList>
+      {/* <div style={{ marginTop: "20px", fontWeight: "bold", color: "#333333", justifyContent: "center", marginRight: "120px" }}>
+        {selectedOption}
+      </div> */}
     </div>
   );
 }
 
 export default SideBar;
+
+// import React, { useState } from "react";
+// import { Route, Routes, useNavigate } from "react-router-dom";
+// import {
+//   ListItemIcon,
+//   ListItemText,
+//   MenuList,
+//   MenuItem,
+//   Typography,
+// } from "@mui/material";
+// import { makeStyles } from "@mui/styles";
+// import { Keyboard, BatchPrediction } from "@mui/icons-material";
+
+// const useStyles = makeStyles({
+//   menuItem: {
+//     "&.css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root.Mui-selected": {
+//       backgroundColor: "#ede7f6",
+//       borderRadius: 8,
+//       fontWeight: "bold",
+//       color: "#5e35b1",
+//       margin: 3,
+//       width: "12vw",
+//     },
+//     "&.css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root:not(.Mui-selected)": {
+//       "&:hover": {
+//         backgroundColor: "#ede7f6",
+//         color: "#5e35b1",
+//         borderRadius: 8,
+//         "& $icon": {
+//           color: "#5e35b1",
+//           fontSize: "initial",
+//         },
+//       },
+//       "& $icon": {
+//         color: "initial",
+//         fontSize: "initial",
+//       },
+//     },
+//   },
+//   icon: {
+//     color: "#5e35b1",
+//   },
+// });
+
+// function SideBar() {
+//   const classes = useStyles();
+//   const navigate = useNavigate();
+
+//   const [selectedOption, setSelectedOption] = useState("Dashboard");
+
+//   const handleOptionClick = (option) => {
+//     setSelectedOption(option);
+//     // Navigate to the respective route based on the selected option
+//     if (option === "Home") {
+//       navigate("/home/main1");
+//     } else if (option === "Dashboard") {
+//       navigate("/home");
+//     }
+//   };
+
+//   return (
+//     <div style={{ position: "fixed", height: "50vh", padding: "15px" }}>
+//       <div style={{ fontWeight: "bold", color: "#333333", marginLeft: -35 }}>
+//         PDS-DASHBOARD
+//       </div>
+//       <MenuList>
+//         <MenuItem
+//           style={{ marginLeft: "2px" }}
+//           onClick={() => handleOptionClick("Home")}
+//           selected={selectedOption === "Home"}
+//           classes={{ root: classes.menuItem }}
+//         >
+//           <ListItemIcon>
+//             <BatchPrediction fontSize="small" className={classes.icon} />
+//           </ListItemIcon>
+//           <ListItemText>
+//             <Typography
+//               style={{
+//                 fontSize: 14,
+//                 marginLeft: -100,
+//                 fontWeight: "bold",
+//                 color: "#333333",
+//               }}
+//               variant="inherit"
+//             >
+//               Home
+//             </Typography>
+//           </ListItemText>
+//         </MenuItem>
+//         <MenuItem
+//           onClick={() => handleOptionClick("Dashboard")}
+//           selected={selectedOption === "Dashboard"}
+//           classes={{ root: classes.menuItem }}
+//         >
+//           <ListItemIcon>
+//             <Keyboard fontSize="small" className={classes.icon} />
+//           </ListItemIcon>
+//           <ListItemText>
+//             <Typography
+//               style={{
+//                 fontSize: 14,
+//                 marginLeft: -75,
+//                 fontWeight: "bold",
+//                 color: "#333333",
+//               }}
+//               variant="inherit"
+//             >
+//               Dashboard
+//             </Typography>
+//           </ListItemText>
+//         </MenuItem>
+//       </MenuList>
+
+//       {/* Display selected option */}
+//       <div style={{ marginTop: "20px", fontWeight: "bold", color: "#333333" }}>
+//         Selected Option: {selectedOption}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SideBar;
