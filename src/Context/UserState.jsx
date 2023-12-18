9dfcb72b29394c54aa534745314c7fe146dd6e2e
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2023-12-18 17:13:48 +0530

    UserState.jsx
    Adding Context Api

--- src/Context/UserState.jsx
+++ src/Context/UserState.jsx
@@ -4,6 +4,20 @@ import UserContext from "./userContext";
 function UserState(props) {
   const [users, setUsers] = useState([]);
 
+  const [counts, setCounts] = useState({});
+  console.log(setCounts);
+
+  const [GraphData , setGraphData] = useState()
+  const [selectedOption, setSelectedOption] = useState('Dashboard');
+// console.log(selectedOption)
+console.log(setSelectedOption);
+
+
+  console.log(setSelectedOption)
+  const addUser = (user) => {
+    setUsers([...users, user]);
+  };
+
   useEffect(() => {
     const apiUrl = "http://127.0.0.1:5000/get_users";
 
@@ -16,9 +30,11 @@ function UserState(props) {
         console.error("Error fetching data:", error);
       });
   }, []);
+  
 
   return (
-    <UserContext.Provider value={{ users, setUsers }}>
+    <UserContext.Provider value={{ users, setUsers, counts , GraphData , setCounts,setGraphData , selectedOption, setSelectedOption ,  addUser}}>
+      
       {props.children}
     </UserContext.Provider>
   );
