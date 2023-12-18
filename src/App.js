180d30e8a244f8dde3a5f7120908f8e20b280f0c
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2023-12-18 17:15:45 +0530

    App.js
    Fixing Routes Paths

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Main2 from "./Components/Main2";
 import Main from "./Components/Main";
 import Error from "./Components/Error";
 
+
+
 function App() {
   return (
     <div className="App">
       <UserState>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/home" element={<Home />}>
+          <Route exact path="/dashboard" element={<Home />}>
             <Route exact path="" element={<Main />} />
-            <Route exact path="main1" element={<Main2 />} />
+            <Route exact path="home" element={<Main2 />} />
           </Route>
           <Route path="/*" element={<Error />} />
         </Routes>
